d34d47ac2a963cc251c226e09b0531a36da4db8d
Anton <B_atonyo_S@bk.ru>
2023-06-10 22:57:39 +0300

    Task43 completed1

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -2,7 +2,7 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 //значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-int[] StringToInt() //возвращает введенные b и k через пробел в массив с проверкой необходимого количества чисел
+double [] StringToInt() //возвращает введенные b и k через пробел в массив с проверкой необходимого количества чисел
 {
     System.Console.WriteLine("Введите b1 k1 b2 k2 через пробел");
     string line = Console.ReadLine();
@@ -13,22 +13,22 @@ int[] StringToInt() //возвращает введенные b и k через 
         line = Console.ReadLine();
         ArrayString = line.Split(" ");
     }
-    int[] result = new int[ArrayString.Length];
-    for (int i = 0; i < 4; i++) result[i] = int.Parse(ArrayString[i]);
+    double [] result = new double[ArrayString.Length];
+    for (int i = 0; i < 4; i++) result[i] = double.Parse(ArrayString[i]);
     return result;
 }
-int [] crossing (int [] array)
+double [] crossing (double [] array)
 {
-    int [] result2 = new int [2];
-    result2 [1] = (array[3]-array[1])/(array[2]-array[4]);
-    result2 [2] = array[2]*result2[1]+array[1];
+    double [] result2 = new double [2];
+    result2 [0] = (array[2]-array[0])/(array[1]-array[3]);
+    result2 [1] = array[1]*result2[0]+array[0];
     return result2; 
 }
-Console.ReadLine();
+Console.Clear();
 System.Console.WriteLine("hi");
-int [] array1 = StringToInt(); 
-int [] result1 = crossing(array1);
-System.Console.WriteLine($"y = {array1[2]}*x + {array1[1]}");
-System.Console.WriteLine($"y = {array1[4]}*x + {array1[3]}");
-System.Console.WriteLine($"точка пересечения уравнений: x = {result1[1]}; y = {result1[2]} ");
+double [] array1 = StringToInt(); 
+double [] result1 = crossing(array1);
+System.Console.WriteLine($"y = {array1[1]:f2}*x + {array1[0]:f2}");
+System.Console.WriteLine($"y = {array1[3]:f2}*x + {array1[2]:f2}");
+System.Console.WriteLine($"точка пересечения уравнений: x = {result1[0]:f2}; y = {result1[1]:f2} ");
 
